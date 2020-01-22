3599a5a1d814d55336e52f04ee7e0c46c9cbd3f3
Jim Ward <seker@uwyo.edu>
2020-01-22 14:54:11 -0700

    fixed typo

--- tuckarm.py
+++ tuckarm.py
@@ -10,8 +10,8 @@ class TuckingClient(object):
 		self.move_group = MoveGroupInterface("arm", "base_link")
 
 	def tuck(self):
-		joints = ["shoulder_pan_join", "shoulder_lift_joint", "upperarm_roll_joint",
-			  "elbow_flex_joint", "forearm_roll_joint", "wrist_flex_joint", "wrist_roll_join"]
+		joints = ["shoulder_pan_joint", "shoulder_lift_joint", "upperarm_roll_joint",
+			  "elbow_flex_joint", "forearm_roll_joint", "wrist_flex_joint", "wrist_roll_joint"]
 		pose = [1.32, 1.40, -0.2, 1.72, 0.0, 1.66, 0.0]
 		while not rospy.is_shutdown():
 			result = self.move_group.moveToJointPosition(joints, pose, 0.02)
